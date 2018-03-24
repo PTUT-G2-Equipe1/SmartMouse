4359da96e57105cdd753bc18641e9081a0c31642
Axel Gatineau Philippe <axel.gatineau-philippe@etu.univ-amu.fr>
2018-03-24 23:36:04 +0100

    Mode nuit début

--- app/src/main/java/com/example/f15003938/smartmouse/Reglages.java
+++ app/src/main/java/com/example/f15003938/smartmouse/Reglages.java
@@ -3,12 +3,18 @@ package com.example.f15003938.smartmouse;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.SimpleAdapter;
+import android.widget.Switch;
+import android.widget.Toolbar;
 
 public class Reglages extends AppCompatActivity {
 
     private Button retour;
+    private Switch modenuit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,35 @@ public class Reglages extends AppCompatActivity {
 
         retour = (Button) findViewById(R.id.retour);
 
+        modenuit = (Switch) findViewById(R.id.ModeNuit);
+        modenuit.setChecked(false);
+        modenuit.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+                }else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+        super.onCreate(savedInstanceState);
+        super.recreate();
+        modenuit.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    recreate();
+
+                }else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    recreate();
+                }
+            }
+        });
+    }
         /**
          * Ecouteur permettant de changer de menu
          */
